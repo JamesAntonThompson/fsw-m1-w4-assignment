547a2378c2e0cc9b765a78325db0776520a5f0bc
james thompson <jamesantonthompson@gmail.com>
2017-09-10 06:50:28 +1000

    assignment 4 - modals working with javascript

--- js/script.js
+++ js/script.js
@@ -1,12 +1,42 @@
-        $("#carousel-button").click(function(){
-            if ($("#carousel-button").children("span").hasClass('fa-pause')) {
-                $("#mycarousel").carousel('pause');
-                $("#carousel-button").children("span").removeClass('fa-pause');
-                $("#carousel-button").children("span").addClass('fa-play');
-            }
-            else if ($("#carousel-button").children("span").hasClass('fa-play')){
-                $("#mycarousel").carousel('cycle');
-                $("#carousel-button").children("span").removeClass('fa-play');
-                $("#carousel-button").children("span").addClass('fa-pause');                    
-            }
-        });
+$("#carousel-button").click(function(){
+    if ($("#carousel-button").children("span").hasClass('fa-pause')) {
+        $("#mycarousel").carousel('pause');
+        $("#carousel-button").children("span").removeClass('fa-pause');
+        $("#carousel-button").children("span").addClass('fa-play');
+    }
+    else if ($("#carousel-button").children("span").hasClass('fa-play')){
+        $("#mycarousel").carousel('cycle');
+        $("#carousel-button").children("span").removeClass('fa-play');
+        $("#carousel-button").children("span").addClass('fa-pause');                    
+    }
+});
+
+$("#loginModalButton").click(function() {
+    // console.log( 'click detected');
+    $('#loginModal').modal('show');
+});
+
+$("#loginModalCancel").click(function() {
+    // console.log( '#loginModalCancel - click detected');
+    $('#loginModal').modal('hide');
+});
+
+$("#loginModalCancelX").click(function() {
+    // console.log( '#loginModalCancelx - click detected');
+    $('#loginModal').modal('hide');
+});
+
+$("#reservationModalButton").click(function() {
+    // console.log( 'click detected');
+    $('#reservationModal').modal('show');
+});
+
+$("#reservationModalCancel").click(function() {
+    console.log( '#reservationModalCancel - click detected');
+    $('#reservationModal').modal('hide');
+});
+
+$("#reservationModalCancelX").click(function() {
+    console.log( '#reservationModalCancelx - click detected');
+    $('#reservationModal').modal('hide');
+});
